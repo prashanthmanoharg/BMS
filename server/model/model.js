26d896be7eb6b5ffacbf6d2505cce75d3bffc7d9
unknown <prashanthmanohar.g@outlook.com>
2022-06-08 10:40:22 +0530

    First Version

---
+++ server/model/model.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+var schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    bankname: {
+        type: String,
+        required: true,
+        
+    },
+
+    branch: {
+        type:String,
+        required:true,
+},
+
+acctype:{
+         type:String,
+          required:true,
+},
+
+    username: {
+          type: String,
+          required: true,
+    },
+
+    password: {
+            type: String,
+            required: true,
+
+    },
+
+    transpasswd:
+    {
+           type:String,
+           required: true,
+    },
+
+    cardno: {
+           type:String,
+           required: true,
+
+
+    },
+
+    bankpin: {
+
+        type:String,
+        required: true,
+    },
+
+
+});
+
+const Userdb = mongoose.model("userdb", schema);
+
+module.exports = Userdb;
